2ef84a595d4371712faf97661d62fc24f7f3b687
larpingston <7some@tutamail.com>
2026-04-08 19:41:08 +0400

    Add files via upload

--- options/schrute/bootloader.sh
+++ options/schrute/bootloader.sh
@@ -10,7 +10,7 @@ option_bootloader() {
     --no-cancel \
     --extra-button --extra-label "MAP" \
     --checklist  "Select bootloader:" 0 0 0 \
-    "grub" "" "on" "limine" "" "off" "refind" "" "off" "efistub" "" "off" "efistub_fallback" "" "off"
+    "limine" "" "on" "refind" "" "off" "efistub" "" "off" "efistub_fallback" "" "off"
     )
         [ "$?" == "3" ] && map
 
